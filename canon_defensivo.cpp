d40377accb0c7e1558db71ed6a967bb38c56f413
lizeth.villa <lizeth.villa@udea.edu.co>
2020-12-12 11:39:44 -0500

    defincion de metodos

--- canon_defensivo.cpp
+++ canon_defensivo.cpp
@@ -1,5 +1,35 @@
 #include "canon_defensivo.h"
 
+float canon_defensivo::getPosicion_inicial_x_canon_defensivo() const
+{
+    return posicion_inicial_x_canon_defensivo;
+}
+
+void canon_defensivo::setPosicion_inicial_x_canon_defensivo(float value)
+{
+    posicion_inicial_x_canon_defensivo = value;
+}
+
+float canon_defensivo::getPosicion_inicial_y_canon_defensivo() const
+{
+    return posicion_inicial_y_canon_defensivo;
+}
+
+void canon_defensivo::setPosicion_inicial_y_canon_defensivo(float value)
+{
+    posicion_inicial_y_canon_defensivo = value;
+}
+
+void canon_defensivo::imprimir_resultados()
+{
+    cout << "impacto posicion x canon defensivo; "<< x <<endl;
+    cout << "impacto posicion y canon defensivo; "<< y <<endl;
+    cout << "impacto velocidad inicial canon defensivo; "<< velocidad_inicial_x_canon_defensivo<<endl;
+    cout << "impacto angulo canon defensivo; "<< angulo_canon_defensivo <<endl;
+    cout << "tiempo de impacto canon defensivo; "<< tiempo_canon_defensivo<<endl;
+
+}
+
 canon_defensivo::canon_defensivo()
 {
 
--- canon_defensivo.h
+++ canon_defensivo.h
@@ -8,11 +8,13 @@ class canon_defensivo
     float  posicion_y_canon_defensivo; //x
     float  posicion_inicial_x_canon_defensivo;//xo
     float  posicion_inicial_y_canon_defensivo;//xo
-    float  velocidad_x_canon_defensivo;
-    float  velocidad_y_canon_defensivo;
+    float  velocidad_inicial_x_canon_defensivo;
+    float  velocidad_inicial_y_canon_defensivo;
     float  tiempo_canon_defensivo;
     float angulo_canon_defensivo;
     float gravedad = 9.81;
+    float distancia;
+    float rango_destrucion_canon_defesivo = distancia*0.05;
 
 public:
     canon_defensivo();
