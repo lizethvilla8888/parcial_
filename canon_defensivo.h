3a9e5fd36f5c54f072a84607fb9b564719258879
lizeth.villa <lizeth.villa@udea.edu.co>
2020-12-12 12:10:51 -0500

    actualizacion de documento de seguimiento

--- canon_defensivo.h
+++ canon_defensivo.h
@@ -1,6 +1,12 @@
 #ifndef CANON_DEFENSIVO_H
 #define CANON_DEFENSIVO_H
 
+#include <iostream>
+#include <math.h>
+
+#include "canon_ofensivo.h"
+
+using namespace std;
 
 class canon_defensivo
 {
@@ -10,6 +16,8 @@ class canon_defensivo
     float  posicion_inicial_y_canon_defensivo;//xo
     float  velocidad_inicial_x_canon_defensivo;
     float  velocidad_inicial_y_canon_defensivo;
+    float velocidad_inicial_canon_defensivo;
+    float x,y; // donde esplota
     float  tiempo_canon_defensivo;
     float angulo_canon_defensivo;
     float gravedad = 9.81;
@@ -18,6 +26,23 @@ class canon_defensivo
 
 public:
     canon_defensivo();
+    canon_defensivo(float );
+
+    float getPosicion_inicial_x_canon_defensivo() const;
+    void setPosicion_inicial_x_canon_defensivo(float value);
+
+    float getPosicion_inicial_y_canon_defensivo() const;
+    void setPosicion_inicial_y_canon_defensivo(float value);
+
+    float getVelocidad_inicial_canon_defensivo() const;
+    void setVelocidad_inicial_canon_defensivo(float value);
+
+     void imprimir_resultados();
+     float getAngulo_canon_defensivo() const;
+     void setAngulo_canon_defensivo(float value);
+
+     float getDistancia() const;
+     void setDistancia(float value);
 };
 
 #endif // CANON_DEFENSIVO_H
